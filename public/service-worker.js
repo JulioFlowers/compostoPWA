68ad3065081204c05ca3e58e318b534d8ab9546d
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-08-18 02:25:32 -0500

    cambios chidos

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,47 +1,43 @@
-'use strict'
-
-const CACHE_NAME = 'composto-Cache';
+const CACHE_NAME = 'composto-cache';
 const toCache = [
   '/',
   '/index.html',
+  '/js/status.js',
 ];
 
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(function(cache) {
-          return cache.addAll(toCache)
-        })
-        .then(self.skipWaiting())
-    )
-  })
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      fetch(event.request)
-        .catch(() => {
-          return caches.open(CACHE_NAME)
-            .then((cache) => {
-              return cache.match(event.request)
-            })
-        })
-    )
-  })
-  
-  elf.addEventListener('activate', function(event) {
-    event.waitUntil(
-      caches.keys()
-        .then((keyList) => {
-          return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-              console.log('[ServiceWorker] Removing old cache', key)
-              return caches.delete(key)
-            }
-          }))
-        })
-        .then(() => self.clients.claim())
-    )
-  })
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(toCache)
+      })
+      .then(self.skipWaiting())
+  )
+})
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then((cache) => {
+            return cache.match(event.request)
+          })
+      })
+  )
+})
 
-
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+      .then(() => self.clients.claim())
+  )
+})
