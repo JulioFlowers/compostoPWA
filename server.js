d1a9183c9c6eb853f7466a79f1626610bc7c847c
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-08-18 01:24:50 -0500

    13

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const fs = require('fs')
 const https = require('https')
 const app = express()
 
-app.use((req, res, next) => {
-  if (!req.secure) {
-    return res.redirect('https://' + req.headers.host + req.url);
-  }
-  next();
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+  return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  else
+    next() /* Continue to other routes if we're not redirecting */
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -18,6 +18,6 @@ app.get('/',(req, res)=> {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
