5f0271cab360c7026b7ba1986e63dbb43ff589f9
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-08-18 00:18:08 -0500

    intento6

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
-const fs = require('fs')
 
-const httpPort = 8080
+const httpPort = 80
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
