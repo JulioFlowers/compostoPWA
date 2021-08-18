7b9911096d81edbc6d4753b44a683da08a068a81
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-08-18 00:27:34 -0500

    intento 7

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const path = require('path')
-
-const httpPort = 80
+const express  = require('express');
+const app      = express();
+const port     = process.env.PORT || 8080;
+const path     = require('path');
 
 const app = express()
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
+
+app.get('/',(req, res)=> {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-app.listen(httpPort, function () {
-  console.log(`Listening on port ${httpPort}!`)
-})
+
+app.listen(port);
+console.log('Servidor iniciado en puerto ' + port);
