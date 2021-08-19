e39ce470aeb7a73e8478651ebf1a8a3e11b291e9
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-08-19 05:10:37 -0500

    pruebavergas

--- server.js
+++ server.js
@@ -1,21 +1,82 @@
 'use strict'
 const express = require('express')
+const passport = require('passport')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passportLocal = require('passport-local').Strategy
 const app = express()
 
 app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname + '/views'))
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser('elperroespacialescuchaañoñocantarflamenco'))
 
+app.use(session({
 
-/*app.get('*',function(req,res,next){
+  secret: 'elperroespacialescuchaañoñocantarlasleyesdeGauß',
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new passportLocal((username, password,done)=>{
+
+  if(username=="researcher" && password=="nitrobacter")
+     return done(null, {id: 1, name: "Perfil Researcher"})
+
+  else if(username=="guest" && password=="composta")
+     return done(null, {id: 2, name: "Perfil Invitado"})
+
+  done(null,false)
+
+}))
+
+passport.serializeUser((user,done)=>{
+
+  done(null, user.id);
+})
+
+passport.deserializeUser((id,done)=>{
+  if (id==1)
+     done(null, {id: 1, name: "Perfil Researcher"} )
+
+  else if(id==2)
+     done(null, {id: 2, name: "Perfil Invitado"})
+
+})
+
+app.get('*',function(req,res,next){
   if(req.headers['x-forwarded-proto']!='https')
   return res.redirect(['https://', req.get('Host'), req.url].join(''));
   else
     next()  //Continue to other routes if we're not redirecting 
-}) */
+})
 
-app.get('/',(req, res)=> {
+app.get('/',(req,res,next)=>{
+
+  if (req.isAuthenticated()) 
+    return next()
+
+  res.redirect('/login')
+
+},(req, res)=> {
+
      res.render('index.ejs');
 })
+
+app.get('/login', (req,res)=>{
+
+  res.render('pages/login.ejs')
+})
+
+app.post('/login',passport.authenticate('local',{
+
+  successRedirect: '/',
+  failureRedirect: '/login'
+}))
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
