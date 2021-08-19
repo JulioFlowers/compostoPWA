5a5ab1d5bd693098c2b211cff9e3c59d55cd6178
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-08-19 02:10:19 -0500

    veamosaversisale

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/views'))
 
+/*
 app.get('*',function(req,res,next){
   if(req.headers['x-forwarded-proto']!='https')
   return res.redirect(['https://', req.get('Host'), req.url].join(''));
   else
     next()  //Continue to other routes if we're not redirecting 
-}) 
+}) */
 
 app.get('/',(req, res)=> {
-     res.render('/index');
+     res.render('index.ejs');
 })
 
 app.listen(process.env.PORT || 3000, function(){
