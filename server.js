2032f74c8e5b56d4fba9a6628c75527a417238c7
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-08-19 02:48:29 -0500

    navbarupdate

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'))
 
 
-app.get('*',function(req,res,next){
+/*app.get('*',function(req,res,next){
   if(req.headers['x-forwarded-proto']!='https')
   return res.redirect(['https://', req.get('Host'), req.url].join(''));
   else
     next()  //Continue to other routes if we're not redirecting 
-}) 
+}) */
 
 app.get('/',(req, res)=> {
      res.render('index.ejs');
