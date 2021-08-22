ebe166f71a6e023c6e560f6bdfb6cf08ecb2c54d
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-08-21 19:42:02 -0500

    arregloschidos

--- server/mqttcon.js
+++ server/mqttcon.js
@@ -6,8 +6,23 @@ module.exports = (mqtt) => {
         host: "34.102.107.56",
         port: 8083,
         protocol: "mqtt",
-        clientId: "composto_monitor"+ ( Math.random() * 3.3)
-      };
-      var client = mqtt.connect(Opciones);
+        clientId: "composto_monitor" + (Math.random() * 3.3)
+    };
+
+    var client = mqtt.connect(Opciones)
+
+    client.on('connect', function () {
+        client.subscribe('presence', function (err) {
+            if (!err) {
+                client.publish('presence', 'Hello mqtt')
+            }
+        })
+    })
+
+    client.on('message', function (topic, message) {
+        // message is Buffer
+        console.log(message.toString())
+        client.end()
+    })
 
 }
