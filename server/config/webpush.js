8713e4e1005f8a0e2adc26dcd999264b4a5e4c60
Julio Flowers <julio_alfonso2000@hotmail.com>
2021-09-05 19:42:39 -0500

    tacos de update

--- server/config/webpush.js
+++ server/config/webpush.js
@@ -1,5 +1,5 @@
 const webpush = require('web-push')
 
-webpush.setVapidDetails('mailto:notifications@compostomonitor.com',process.env.COMPOSTOPU_VAPID_KEY, process.env.COMPOSTOPR_VAPID_KEY)
+webpush.setVapidDetails('mailto:pnot@compostomonitor.com',process.env.COMPOSTOPU_VAPID_KEY, process.env.COMPOSTOPR_VAPID_KEY)
 
 module.exports = webpush
